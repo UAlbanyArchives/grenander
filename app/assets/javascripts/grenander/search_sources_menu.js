7e86b7e138d8445b9930988aabb27a1720407af7
Greg Wiedeman <gregory.wiedeman1@gmail.com>
2021-04-15 15:26:16 -0400

    book facet search option links now work

--- app/assets/javascripts/grenander/search_sources_menu.js
+++ app/assets/javascripts/grenander/search_sources_menu.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function(){
-	var pathname = window.location.pathname.toLowerCase() + window.location.search.toLowerCase();
-	// console.log(pathname);
+	var pathnameEncoded = window.location.pathname.toLowerCase() + window.location.search.toLowerCase();
+	var pathname = decodeURI(pathnameEncoded);
+	console.log(pathname);
 	var sourcesMenu = document.getElementById("search-sources-menu");
 	if (sourcesMenu) {
 
@@ -10,7 +11,8 @@ document.addEventListener("DOMContentLoaded", function(){
 		} 
 
 		var searchLabel = document.getElementsByClassName("search-intro");
-
+		console.log(pathname);
+		console.log(pathname.startsWith("/books"));
 		if (pathname.startsWith("/search")) {
 			var target = sourcesMenu.getElementsByClassName("search");
 		} else if (pathname.startsWith("/description")) {
@@ -19,11 +21,11 @@ document.addEventListener("DOMContentLoaded", function(){
 			var target = sourcesMenu.getElementsByClassName("catalog");
 		} else if (pathname.startsWith("/history")) {
 			var target = sourcesMenu.getElementsByClassName("history");
-		} else if (pathname.includes("f[collecting_area_ssim][]=Mathes+Childrens+Literature")) {
+		} else if (pathname.includes("f[collecting_area_ssim][]=mathes+childrens+literature")) {
 			var target = sourcesMenu.getElementsByClassName("mathes");
 			searchLabel[0].innerHTML = "Mathes Childrens Literature";
-		} else if (pathname.includes("f[collecting_area_ssim][]=Political+Pamphlets")) {
-			var target = sourcesMenu.getElementsByClassName("mathes");
+		} else if (pathname.includes("f[collecting_area_ssim][]=political+pamphlets")) {
+			var target = sourcesMenu.getElementsByClassName("pamphlets");
 			searchLabel[0].innerHTML = "Political Pamphlets";
 		} else if (pathname.startsWith("/books")) {
 			var target = sourcesMenu.getElementsByClassName("books");
